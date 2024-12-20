e2f4d27cca5e847268a80bb38544483fe0046533
Cameron Cook <Cameron.cook0310@gmail.com>
2024-12-20 13:43:24 -0600

    Implement sketch screen grid size variability

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-// Create initial page structure of 16 by 
+// Create initial screen grid structure of 16 by 16 divs
 const container = document.querySelector(".content-container");
+let enterCount = 0;
 
 for (i=0; i<16; i++) {
     let rowDiv = document.createElement("div");
@@ -14,11 +15,44 @@ for (i=0; i<16; i++) {
     container.append(rowDiv);
 }
 
-let enterCount = 0;
+function deleteScreenGrid() {
+    let lastChild = container.lastElementChild;
+    while (lastChild.tagName == "DIV") {
+        container.removeChild(container.lastElementChild);
+        lastChild = container.lastElementChild;
+    }
+}
+
+function updateScreenGrid(newSize) {
+    deleteScreenGrid()
+
+    let sizeRatio = 16 / newSize
+
+    for (i=0; i<newSize; i++) {
+        let rowDiv = document.createElement("div");
+        rowDiv.classList.toggle("div-row");
+        rowDiv.id = `row-div-${i}`;
+        for (j=0; j<newSize; j++) {
+            let div = document.createElement("div");
+            div.id = `div-${i}-${j}`;
+            div.classList.toggle("grid-div");
+            div.style.height = `${sizeRatio * 40}px`
+            div.style.width = `${sizeRatio * 40}px`
+            rowDiv.appendChild(div);
+        }
+        container.append(rowDiv);
+    }
+    enterCount = 0;
+}
+
+
 
 document.addEventListener("mouseover", (e) => {
     let target = e.target;
+
+    // Detects when element entered by the mouse is a part of the screen grid
     if (target.classList.contains("grid-div")) {
+        // Handles color changing of divs entered
         let currentColor = "#bcb6b6";
         if (enterCount < 100) {
             enterCount += 10;
@@ -32,5 +66,18 @@ document.addEventListener("mouseover", (e) => {
         target.style.backgroundColor = `color-mix(in oklab, #4f4d4e ${enterCount.toString()}%, ${currentColor}`;
     } else {
         enterCount = 0;
+    }
+});
+
+document.addEventListener("click", (e) => {
+    let target = e.target;
+    
+    if (target.id == "title") {
+        // Gets new grid size from user and validates input
+        let gridSize = prompt("Enter new grid size between 1 and 100");
+        while (!Number(gridSize) || Number(gridSize) < 1 || Number(gridSize) > 100) {
+            gridSize = prompt("Invalid grid size. Enter new grid size between 1 and 100");
+        }
+        updateScreenGrid(gridSize);
     }
 });
