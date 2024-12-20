ed97ddcfcb3c65e08f2a0874678950df1998ed62
Cameron Cook <Cameron.cook0310@gmail.com>
2024-12-20 09:18:16 -0600

    Implement div grid structure

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-// Create initial page structure
+// Create initial page structure of 16 by 
+const container = document.querySelector(".content-container");
+
+for (i=0; i<16; i++) {
+    let rowDiv = document.createElement("div");
+    rowDiv.classList.toggle("div-row");
+    rowDiv.id = `row-div-${i}`;
+    for (j=0; j<16; j++) {
+        let div = document.createElement("div");
+        div.id = `div-${i}-${j}`;
+        div.classList.toggle("grid-div");
+        rowDiv.appendChild(div);
+    }
+    container.append(rowDiv);
+}
